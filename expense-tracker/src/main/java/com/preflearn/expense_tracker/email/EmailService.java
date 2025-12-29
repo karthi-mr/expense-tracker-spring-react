e3adc13fd5dc8c291f1370c23a01da94ed5c1585
Karthi-mr <mkarthim1998@gmail.com>
2025-12-29 21:18:10 +0530

    implemented email service

---
+++ expense-tracker/src/main/java/com/preflearn/expense_tracker/email/EmailService.java
@@ -0,0 +1,70 @@
+package com.preflearn.expense_tracker.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Value("${application.mail.from}")
+    private String fromEmail;
+
+    @Async
+    public void sendEmail(
+          String to,
+          String username,
+          EmailTemplateName emailTemplateName,
+          String confirmationUrl,
+          String activationCode,
+          String subject
+    ) throws MessagingException {
+        final String templateName;
+
+        if (emailTemplateName == null) {
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplateName.getName();
+        }
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MULTIPART_MODE_MIXED,
+                StandardCharsets.UTF_8.name()
+        );
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activationCode", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        helper.setFrom(this.fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = this.templateEngine.process(templateName, context);
+        helper.setText(template, true);
+        this.mailSender.send(mimeMessage);
+    }
+}
