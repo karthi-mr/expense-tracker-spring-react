4cfd31e3050556c7361de963d845d355f440e256
Karthi-mr <mkarthim1998@gmail.com>
2026-01-07 16:34:43 +0530

    initial react commit

---
+++ expense-tracker-ui/src/App.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from "react";
+
+export default function App (): ReactElement {
+  return (
+    <h1 className="text-5xl text-red-300">Welcome to Expense Tracker</h1>
+  );
+}
